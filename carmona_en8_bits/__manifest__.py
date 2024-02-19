d132ec1091e3292dbb8b9666656ccb18e3ea769b
Juanma Carmona <juanmacarmona97@gmail.com>
2024-02-19 18:34:29 +0100

    Termino modulo odoo del proyecto de videojuego de rol

--- carmona_en8_bits/__manifest__.py
+++ carmona_en8_bits/__manifest__.py
@@ -23,9 +23,12 @@
     # always loaded
     'data': [
         'security/ir.model.access.csv',
-        'views/views.xml',
-        'views/templates.xml',
         'views/usuarios.xml',
+        'views/cuentas.xml',
+        'views/misiones.xml',
+        'views/personajes.xml',
+        'views/objetos.xml',
+        'views/regiones.xml'
     ],
     # only loaded in demonstration mode
     'demo': [
--- carmona_en8_bits/models/models.py
+++ carmona_en8_bits/models/models.py
@@ -22,22 +22,7 @@ class usuarios(models.Model):
                 # Verificar que el DNI tenga exactamente 9 caracteres
                 if len(record.dni) != 9:
                     raise ValidationError("El DNI debe tener 9 caracteres")
-                
-                # Verificar que el DNI contenga solo números
-                if not record.dni.isdigit():
-                    raise ValidationError("El DNI solo puede contener números")
-                
-                # Verificar el formato del DNI
-                dni_pattern = r'^[0-9]{8}[A-Za-z]$'
-                if not re.match(dni_pattern, record.dni):
-                    raise ValidationError("Formato de DNI no válido")
-                
-                # Verificar la letra del DNI
-                letra = record.dni[-1].upper()
-                letras_validas = 'TRWAGMYFPDXBNJZSQVHLCKE'
-                dni_numeros = int(record.dni[:-1])
-                if letra != letras_validas[dni_numeros % 23]:
-                    raise ValidationError("Letra del DNI incorrecta")
+
                 
 class cuentas(models.Model):
     _name = 'carmona_en8_bits.cuentas'
@@ -47,7 +32,9 @@ class cuentas(models.Model):
     idCuenta = fields.Integer() 
     nombreCuenta = fields.Text(string="nombre de la cuenta asociada al usuario")
     contrasena = fields.Text(string="contraseña de la cuenta")
-    personajesCuenta = fields.One2many("carmona_en8_bits.personajes","cuenta")
+    personajesCuenta = fields.One2many("carmona_en8_bits.personajes","cuenta", string="Personajes de la cuenta")
+    usuario = fields.Many2one('carmona_en8_bits.usuarios', string="Usuario dueño de la cuenta")
+    
 
 class personajes(models.Model):
     _name = 'carmona_en8_bits.personajes'
@@ -59,9 +46,9 @@ class personajes(models.Model):
     cuenta = fields.Many2one("carmona_en8_bits.cuentas")
     objetos = fields.Many2many("carmona_en8_bits.objetos")
     misiones = fields.Many2many("carmona_en8_bits.misiones")
-    clase = fields.Selection([('Guerrero', 'Guerrero'), ('Mago', 'gasolina'), ('Picaro', 'diesel'), ('Picaro', 'diesel'), ('Picaro', 'diesel'), ('Picaro', 'diesel'), ('Picaro', 'diesel')], string='Tipo de motor')("Guerrero","Mago","Clerigo","Picaro","Druida","Bardo","Brujo")
+    clase = fields.Selection([('Guerrero', 'Guerrero'), ('Mago', 'Mago'), ('Clerigo', 'Clerigo'), ('Picaro', 'Picaro'), ('Druida', 'Druida'), ('Bardo', 'Bardo'), ('Brujo', 'Brujo')], string='Clase del personaje')
     nivel = fields.Integer(string="Nivel",size = 100)
-    raza = fields.enum("Elfo","Humano","Enano","Mediano","Gnomo","Trasgo","Orco","Drow")
+    raza = fields.Selection([('Elfo', 'Elfo'), ('Humano', 'Humano'), ('Enano', 'Enano'), ('Mediano', 'Mediano'), ('Gnomo', 'Gnomo'), ('Trasgo', 'Trasgo'), ('Orco', 'Orco')], string='Raza del personaje') 
     HP = fields.Integer(string="Puntos de vida", readonly=True)
     ATK = fields.Integer(string="Puntos de ataque", readonly=True)
     DEF = fields.Integer(string="Puntos de defensa", readonly=True)
@@ -69,7 +56,7 @@ class personajes(models.Model):
     @api.onchange('clase', 'nivel')
     def _compute_stats(self):
         for record in self:
-            if record.clase in ['mago', 'picaro', 'brujo', 'druida']:
+            if record.clase in ['Mago', 'Picaro', 'Brujo', 'Druida']:
                 record.ATK = record.nivel * 7
                 record.DEF = record.nivel * 3
                 record.HP = record.nivel * 120
@@ -88,7 +75,7 @@ class objetos(models.Model):
     objetosPersonajes =  fields.Many2many("carmona_en8_bits.personajes")
     descripcion = fields.Text("Para que sirve el objeto")
     imagenObjeto = fields.Binary(string="Imagen del objeto")
-    tipo = fields.enum("Consumible","Equipo","Artefacto")
+    tipo = fields.Selection([('Consumible', 'Consumible'), ('Equipo', 'Equipo'), ('Artefacto', 'Artefacto')])
     precio = fields.Float("Valor del mercado del objeto")
     intercambiable = fields.Boolean("Indica si el objeto puede ser traspasado a otro jugador")
 
@@ -103,14 +90,14 @@ class misiones(models.Model):
     region = fields.Many2one("carmona_en8_bits.regiones")
     descripcion = fields.Text(string="Indica el objetivo de la mision")
     recompensas = fields.Text(string="Recompensa de la mision")
-    estado = fields.enum("Pendiente","Completada")
+    estado = fields.Selection([('Pendiente', 'Pendiente'), ('En curso', 'En curso'), ('Completada', 'Completada')])
 
 class regiones(models.Model):
     _name = 'carmona_en8_bits.regiones'
     _description = 'carmona_en8_bits.regiones'
     _rec_name = "nombreRegion"
 
-    nombreRegion = fields.enum("Northel","Surthal","Esthil","Oesthul","Capithol")
-    clima = fields.enum("Sol","Lluvia","Nieve")
+    nombreRegion = fields.Selection([("Northel", "Northel"),("Surthal", "Surthal"),("Esthil", "Esthil"),("Oesthul", "Oesthul"),("Capithol", "Capithol")])
+    clima = fields.Text(string="Clima habitual de la region")
     cantidadMazmorras = fields.Integer(string="cantidad de mazmorras de la region")
     misiones = fields.One2many("carmona_en8_bits.misiones","region")
